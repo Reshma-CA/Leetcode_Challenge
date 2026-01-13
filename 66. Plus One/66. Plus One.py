483950f6f3e068cb74ff0fda917585af24cf2f06
Reshma.CA <reshmacaci@gmail.com>
2026-01-13 13:27:15 +0530

    plus One

---
+++ 66. Plus One/66. Plus One.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = int("".join(map(str, digits)))
+        num += 1
+        return list(map(int, str(num)))
+        
