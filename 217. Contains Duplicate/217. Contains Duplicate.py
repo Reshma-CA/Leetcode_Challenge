adc89e764cc7eee32053db42df1f1731ff7b063f
Reshma.CA <reshmacaci@gmail.com>
2026-01-13 13:17:00 +0530

    Two Sum.py

---
+++ 217. Contains Duplicate/217. Contains Duplicate.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for item in nums:
+            if item in seen:
+                return True
+            
+            seen.add(item)
+        return False
+        
